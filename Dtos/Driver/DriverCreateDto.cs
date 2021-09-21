6944325840ff375b3cb3e53bdce86cfe4bd765f9
aleksandarbalnozan <balnozan@live.com>
2021-09-21 13:34:11 +0200

    Added DataTransferObjects

---
+++ Dtos/Driver/DriverCreateDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrangeTaxi.Dtos.Driver
+{
+    public class DriverCreateDto
+    {
+    }
+}
---
+++ Dtos/Rider/RiderCreateDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrangeTaxi.Dtos.Rider
+{
+    public class RiderCreateDto
+    {
+    }
+}
